8b8b40659fa4fd05cabf225657ed278ca2922e69
ClaraKuper <Clara-Kuper@web.de>
2019-05-20 11:07:23 +0200

    sanity check data

--- analysis/Ana_CompMov.R
+++ analysis/Ana_CompMov.R
@@ -42,7 +42,42 @@ for (trial in 1:nrow(all_des)){
   }
 }
 
+
+#### percent correct as function of reaction time
 for (distance in unique(all_resp$correct)){
   id = which(all_resp$correct == distance)
   hist(all_des$distance[id]) 
 }
+
+timewin = 20
+steps = seq(-0.1,0.5,0.05)
+start = 0
+outmat = matrix(nrow=length(steps), ncol = 2)
+all_resp$correct <- as.numeric(all_resp$correct)
+
+for (step in steps){
+  idx_time <- which(all_resp$t_response>=start & all_resp$t_response<=step)
+  samples <- length(idx_time)
+  percentage <- sum(all_resp$correct[idx_time])/samples
+  outmat[which(steps == step),1] <- step
+  outmat[which(steps == step),2] <- percentage
+  start = step
+}
+
+plot(outmat[,1],outmat[,2])
+
+#### percent correct as function of goal distance
+steps_dis = seq(2,9)
+start_dis = 0
+outmat_dis = matrix(nrow=length(steps_dis), ncol = 2)
+
+for (step in steps_dis){
+  idx_time <- which(all_des$distance>=start_dis & all_des$distance<=step)
+  samples <- length(idx_time)
+  percentage <- sum(all_resp$correct[idx_time])/samples
+  outmat_dis[which(steps_dis == step),1] <- step
+  outmat_dis[which(steps_dis == step),2] <- percentage
+  start_dis = step
+}
+
+plot(outmat_dis[,1],outmat_dis[,2])
