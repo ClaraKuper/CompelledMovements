884d8fe7fd70cfae7f49b983a9171b719d76fb92
ClaraKuper <Clara-Kuper@web.de>
2019-05-29 14:25:03 +0200

    analysis

--- analysis/Ana_CompMov2.R
+++ analysis/Ana_CompMov2.R
@@ -1,4 +1,4 @@
-### This is the analysis of the Compelled Movements Football task ###
+########### Analysis Compelled Movements Direction Change   #########
 ### Clara Kuper 2019 ################################################
 #####################################################################
 ### Goals: ##########################################################
@@ -6,6 +6,8 @@
 ### Sanity check: response times as a function of distance
 
 ########## Set wd and co ###########################################
+setwd("~/Projects/CompelledMovements/analysis")
+
 data_path <- '../data/'
 figure_path <- '../figures/'
 
@@ -13,8 +15,8 @@ figure_path <- '../figures/'
 
 
 ########## Import Data #############################################
-ck01 <- read.table(paste(data_path,'ck1.txt', sep = ''), header = TRUE)
-des_ck01 <- read.table(paste(data_path,'desck1.txt', sep = ''), header = TRUE)
+ck01 <- read.table(paste(data_path,'CoMo2ck1.txt', sep = ''), header = TRUE)
+des_ck01 <- read.table(paste(data_path,'CoMo2desck1.txt', sep = ''), header = TRUE)
 
 
 all_resp <- ck01
@@ -22,9 +24,30 @@ all_des <- des_ck01
 
 ######### Preview Plots ###########################################
 ## Reaction times as function of distance
-plot(all_des$distance,all_resp$t_response, ylim=c(-0.0,0.5))
+plot(all_des$change_loc,all_resp$t_response, ylim=c(-0.0,0.5))
+
+######## Add reverse responses ####################################
 for (trial in 1:nrow(all_des)){
-  if (all_des$direction[trial] == '-'){
+  if (all_des$change[trial] == 1){
+    if(all_des$direction[trial] == '-'){
+    all_des$rev_sign[trial] <- '+'   
+    }
+    else{
+    all_des$rev_sign[trial] <- '-'  
+    }
+  }
+  else {
+    if(all_des$direction[trial] == '-'){
+      all_des$rev_sign[trial] <- '-'   
+    }
+    else{
+      all_des$rev_sign[trial] <- '+'  
+    }
+  }
+}
+
+for (trial in 1:nrow(all_des)){
+  if (all_des$rev_sign[trial] == '-'){
     if(all_resp$response[trial] == 'n'){
       all_resp$correct[trial] <- 0
     }
@@ -32,7 +55,7 @@ for (trial in 1:nrow(all_des)){
       all_resp$correct[trial] <- 1
     }
   }
-  else if (all_des$direction[trial] == '+') {
+  else if (all_des$rev_sign[trial] == '+') {
     if(all_resp$response[trial] == 'n'){
       all_resp$correct[trial] <- 1
     }
@@ -44,9 +67,9 @@ for (trial in 1:nrow(all_des)){
 
 
 #### percent correct as function of reaction time
-for (distance in unique(all_resp$correct)){
-  id = which(all_resp$correct == distance)
-  hist(all_des$distance[id]) 
+for (resp in unique(all_resp$correct)){
+  id = which(all_resp$correct == resp)
+  hist(all_des$change_loc[id]) 
 }
 
 timewin = 20
@@ -67,12 +90,31 @@ for (step in steps){
 plot(outmat[,1],outmat[,2])
 
 #### percent correct as function of goal distance
-steps_dis = seq(2,9)
+steps_dis = seq(-2,-9)
 start_dis = 0
 outmat_dis = matrix(nrow=length(steps_dis), ncol = 2)
 
 for (step in steps_dis){
-  idx_time <- which(all_des$distance>=start_dis & all_des$distance<=step)
+  idx_time <- which(all_des$change_loc<=start_dis & all_des$change_loc>=step)
+  samples <- length(idx_time)
+  percentage <- sum(all_resp$correct[idx_time])/samples
+  outmat_dis[which(steps_dis == step),1] <- step
+  outmat_dis[which(steps_dis == step),2] <- percentage
+  start_dis = step
+}
+
+plot(outmat_dis[,1],outmat_dis[,2])
+
+#### percent correct as function of processing time
+all_resp$ePT <- (all_resp$t_change-all_resp$t_response) * all_des$change
+
+steps_dis = seq(-0.1,1,0.1)
+start_dis = -0.2
+outmat_dis = matrix(nrow=length(steps_dis), ncol = 2)
+
+for (step in steps_dis){
+  idx_time <- which(all_resp$ePT >= start_dis & all_resp$ePT <= step)
+  print(idx_time)
   samples <- length(idx_time)
   percentage <- sum(all_resp$correct[idx_time])/samples
   outmat_dis[which(steps_dis == step),1] <- step
