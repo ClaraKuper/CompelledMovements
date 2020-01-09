fba9176174786a4ea9bdb1e1bb5346482fca3c0d
Clara Kuper <clara-kuper@web.de>
2020-01-09 16:54:08 +0100

    tit and tat

--- CMP_Hand/Functions/correctResponses.m
+++ CMP_Hand/Functions/correctResponses.m
@@ -1,23 +1,39 @@
 % Define correct and incorrect responses from script
 alljumpTime = [];
 allHits     = [];
+
 for b = 1:length(data.block)
   block = data.block(b);
   for t = 1:length(block.trial)
+    hit         = 0;
     trial   = block.trial(t);
     des     = design.b(b).trial(t);
     goalPos = des.goalPos;
     switch goalPos
       case goalPos == trial.resPos
-        trial.hit = True;
-    case goalPos != trial.resPos
-        trial.hit = False;
+        hit = 1;
+      case goalPos != trial.resPos
+        hit = 0;
     end
-    alljumpTime = [alljumpTime, des.jumpTime];
-    allHits     = [allHits, trial.hit];
+    alljumpTime = [alljumpTime, des.jumpTim];
+    allHits     = [allHits, hit];
   end
 end 
 
-scatter(alljumpTime,allHits)
+% scatter(alljumpTime,allHits)
+
 % Clean Data
 
+% As moving mean
+step = max(alljumpTime)/25;
+plot_data = [];
+start = 0.05;
+while start < max(alljumpTime)
+  stop        = start + step;
+  sum_correct = sum(allHits(find(start<alljumpTime<stop)));
+  hit_number  = length(allHits(find(start<alljumpTime<stop)));
+  plot_data   = [plot_data, [start;sum_correct/hit_number]];
+  start       = stop;
+end
+
+plot(plot_data(1,:),plot_data(2,:));
